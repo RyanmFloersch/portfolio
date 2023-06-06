7e885513f161fc2192dd122973d15283212ca07b
Ryan <ryanmfloersch@gmail.com>
2023-06-06 15:40:28 -0400

    Added a link in navigation to pull up my resume

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -10,7 +10,7 @@ function Navigation() {
         <div className="flex md:flex-row flex-col md:justify-start   ">
             <div className="flex flex-row justify-between items-center px-2   ">
 
-                <h1 className="tracking-widest hover:underline underline-offset-2 decoration-emerald-700 text-emerald-200" >Ryan Floersch</h1>
+                <a href="/" className="tracking-widest hover:underline underline-offset-2 decoration-emerald-700 text-emerald-200" >Ryan Floersch</a>
                 {/* drop down menu button and svg image for the buttons icon */}
                 <button data-collapse-toggle="navbar-default" type="button" className="  inline-flex w-10 items-center p-2 ml-3 text-sm text-gray-500 rounded-lg bg-emerald-200 md:hidden  hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 my-3 " aria-controls="navbar-default" area-expanded="false">
                     <span className="sr-only">Open main menu</span>
@@ -27,6 +27,7 @@ function Navigation() {
                     {/* <NavLink className={"block text-black py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"} to="/portfolio">Portfolio</NavLink> */}
                     {/* <NavLink className={"block text-black py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"} to="/contact">Contact</NavLink> */}
                     <a href="mailto: ryanmichaelfloersch@gmail.com" className="block text-black py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</a>
+                    <a href="Ryan Floersch Resume.pdf" className="block text-black py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Resume</a>
                     {/* <NavLink className={"block text-black py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"} to="/resume">Resume</NavLink> */}
                 </ul>
             </div>
