276428ab425bc92a03b63fc5824e7fd93d843383
Ryan <ryanmfloersch@gmail.com>
2023-06-06 14:25:13 -0400

    updated the server.js file to allow it to run the react build when the start command is ran from the server

--- server/server.js
+++ server/server.js
@@ -1,13 +1,32 @@
 require('dotenv').config();
+const http = require('http');
 const express = require('express');
 
 // const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3333;
-
 const app  = express();
+const path = require('path');
 
-app.use(express.static('../client/dist'));
+const _dirname = path.dirname("");
+const buildPath = path.join(_dirname, "../client/build")
+
+app.use(express.static(buildPath));
+
+app.get("/*", function(req, res){
+
+    res.sendFile(
+        path.join(__dirname,"../client/build/index.html"),
+        function(err){
+            if(err){
+                res.status(500).send(err);
+            }
+        }
+    );
+
+});
+
+
 
 app.listen(PORT, () => console.log('Server started on port %s',PORT))
 
