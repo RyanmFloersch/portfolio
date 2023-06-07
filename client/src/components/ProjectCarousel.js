2aab3c4c5551239e2b22db939718d7c5195080dd
Ryan <ryanmfloersch@gmail.com>
2023-06-07 16:51:03 -0400

    Corrected the placedment of the useEffect

--- client/src/components/ProjectCarousel.js
+++ client/src/components/ProjectCarousel.js
@@ -8,15 +8,17 @@ import { Carousel } from 'flowbite-react';
 import CarsoulProjectItem from './CarouselProjectItem';
 
 import { initFlowbite } from 'flowbite'
-useEffect(() => {
-    initFlowbite();
-  }, []);
+
 
 
 function ProjectCarousel(prop) {
     const project_1 = 'This application is a simple client-side application that allows the user to insert a location of their choosing and be provided with four different bowling locations with the name of the establishment, the address, the distance from the starting point, and the phone number. This project was made with group members to build an application using third-party APIs so that we could apply our new acquired skills to the test. We used team collaboration, agile development, storytelling, and project demonstration.';
     const project_2 = 'A web application made to allow users to post and favorite house listings. This application makes use of express to route the information of the application while also making use of sequelize to create the models that would store the user and house data in the sql database.';
     const project_3 = 'Daedalus Roster, is a user-friendly job board application designed to connect talented individuals with exciting job opportunities. Whether you\'re a freelancer looking for project-based work or a user seeking skilled professionals. Freelancers can effortlessly create and publish detailed job listings, showcasing their expertise, availability, and desired compensation. Meanwhile, users can explore a vast array of job listings, filtering by industry, skillset, location, and more.';
+
+    useEffect(() => {
+        initFlowbite();
+      }, []);
 
     return (
 
